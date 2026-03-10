5d34b6b97a5c6815f79c926223a9a2d333227633
Marco Paes <87667389+Markhetos@users.noreply.github.com>
2026-03-10 11:34:40 -0700

    Fixed names in Ticket object

--- pa7-agent-main/agent.py
+++ pa7-agent-main/agent.py
@@ -276,7 +276,7 @@ def book_ticket(user_name: str, movie_title: str):
 
     # Create Ticket obj
     new_ticket = Ticket(
-        user_name=user_profile.name,
+        user_name=user_name,
         movie_title=movie_title,
         time=movie.start_time
     )
